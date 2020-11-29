8e359a49da872a76781e9e0b1e9f49497fc9f86b
Maayan Kestenberg <maayan10.k@gmail.com>
2020-11-30 00:28:50 +0200

    Fix bug for dim=1 csv generation.

--- gen_csv.py
+++ gen_csv.py
@@ -7,10 +7,9 @@ import random
 dim = int(sys.argv[1]);
 rows = int(sys.argv[2]);
 
-for i in range(dim * rows):
-    if i % dim == 1:
-        c = '\n'
-    else:
-        c = ','
-    f = random.uniform(-500.0, 500.0)
-    print("{:.2f}{}".format(f, c), end='')
+def random_float():
+	return random.uniform(-500.0, 500.0)
+def random_float_formatted():
+	return "{:.2f}".format(random_float())
+for i in range(rows):
+	print(",".join(random_float_formatted() for _ in range(dim)))
