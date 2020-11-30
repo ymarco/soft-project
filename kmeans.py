372f73a3979e77f257e4ae067a068a4675c04c11
Maayan Kestenberg <maayan10.k@gmail.com>
2020-11-30 22:45:20 +0200

    Comment debugging lines in python implementation.

--- kmeans.py
+++ kmeans.py
@@ -6,8 +6,7 @@ parser.add_argument('N', type=int)
 parser.add_argument('d', type=int)
 parser.add_argument('MAX_ITER', type=int)
 
-# Todo: remove this line and dprints definition and calls
-parser.add_argument('--verbose','-v','--debug','-d', action='count')
+#parser.add_argument('--verbose','-v','--debug','-d', action='count')
 
 args = parser.parse_args()
 num_clusters=args.K
@@ -25,48 +24,47 @@ soft_assert(num_clusters<num_samples,"K must be smaller than N.")
 soft_assert(dim>0,"d must be a positive integer (bigger than 0).")
 soft_assert(max_iter>=0, "MAX_ITER must be a non-negative integer.")
 
-# Todo: remove
-import sys
-debug=args.verbose
-def dprint(*args):
-	if debug:
-		print(*args, file=sys.stderr)
+#import sys
+#debug=args.verbose
+#def dprint(*args):
+#	if debug:
+#		print(*args, file=sys.stderr)
 
 #dprint(num_clusters, num_samples, dim)
 
 def parseToVec(line):
 	ret = list(map(float,line.split(',')))
 	#dprint(ret)
-	assert len(ret)==dim # Todo: remove all asserts
+	#assert len(ret)==dim 
 	return ret
 
 
 def get_samples_from_stdin():
 	samples = []
 
-	i=0
+	#i=0
 	while True:
 		try:
 			samples.append(parseToVec(input()))
 		except EOFError:
 			break
-		i+=1 # TODO: remove this counter
-	assert i==num_samples
+		#i+=1 
+	#assert i==num_samples
 	return samples
 
 def squared_distance(vec1,vec2):
-	assert len(vec1)==len(vec2)==dim
+	#assert len(vec1)==len(vec2)==dim
 	return sum((vec1[i]-vec2[i])**2 for i in range(dim))
 
 def closest_centroid_ind(sample,centroids):
 	return min(range(num_clusters),key=lambda i:squared_distance(sample,centroids[i]))
 
 def vecs_sum_iter(vecs):
-	assert all(len(vec)==dim for vec in vecs)
+	#assert all(len(vec)==dim for vec in vecs)
 	return map(sum,zip(*vecs))
 
-def vec_div_iter(vec, divisor):
-	return map(lambda x:x/divisor,vec)
+def vec_div_iter(vec, denominator):
+	return map(lambda x:x/denominator,vec)
 
 def vecs_mean_iter(vecs):
 	return vec_div_iter(vecs_sum_iter(vecs),len(vecs))
@@ -83,7 +81,6 @@ def formatVec(vec):
 all_samples = get_samples_from_stdin()
 centroids = all_samples[:num_clusters]
 
-
 clusters=None
 for _ in range(max_iter):
 	old_clusters=clusters
@@ -92,16 +89,17 @@ for _ in range(max_iter):
 		closest_cluster=clusters[closest_centroid_ind(sample,centroids)]
 		closest_cluster.append(sample)
 
+	#dprint(f"clusters = {clusters}\nold_clusters = {old_clusters}")
+
 	# There is a one-to-one correspondence between the clusters and the centroids
 	# (the clusters are a function of the centroids and the constant samples, and
 	# the centroids are a function of the clusters). Thus we can check
 	# for change in the clusters instead of in the centroids.
-	dprint(f"clusters = {clusters}\nold_clusters = {old_clusters}")
 	if clusters==old_clusters: # Deep comparison
 		break
 	for i,cluster in enumerate(clusters):
-		assert len(cluster)>0
+		#assert len(cluster)>0
 		centroids[i]=vecs_mean(cluster)
-	dprint(f"centroids = {centroids}")
+	#dprint(f"centroids = {centroids}")
 for centroid in centroids:
 	print(formatVec(centroid))
