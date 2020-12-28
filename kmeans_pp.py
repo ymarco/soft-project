2c5f0773429fca7f9e4b37bf4b89d4d5eb1b8bc1
Maayan Kestenberg <maayan10.k@gmail.com>
2020-12-29 01:40:04 +0200

    Improve algorithm to use numpy functions only.
    The algorithm now utilizes numpy and its broadcasting feature, which
    should improve performance.

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -1,5 +1,5 @@
 import argparse, random, pandas, numpy as np
-import mykmeanssp as mks
+#import mykmeanssp as mks
 
 parser = argparse.ArgumentParser()
 parser.add_argument("K", type=int)
@@ -29,26 +29,17 @@ soft_assert(num_clusters < num_samples, "K must be smaller than N.")
 soft_assert(dim > 0, "d must be a positive integer (bigger than 0).")
 soft_assert(max_iter >= 0, "MAX_ITER must be a non-negative integer.")
 
-
-def squared_distances(vec, vecs):
-    return np.sum((vecs - vec) ** 2, axis=1)
-
-
-def min_squared_distance(vec, vecs):
-    return np.amin(squared_distances(vec, vecs))
-
-
 RANDOMIZATION_SEED = 0
 np.random.seed(RANDOMIZATION_SEED)
 samples = pandas.read_csv(input_filename, header=None).to_numpy()
 centroid_inds = [np.random.choice(num_samples)]
 for _ in range(num_clusters - 1):
-    centroids = np.array([samples[i] for i in centroid_inds])
-    weights = np.fromiter(
-        (min_squared_distance(sample, centroids) for sample in samples), float
-    )
-    weights /= np.sum(weights)
-    centroid_inds.append(np.random.choice(num_samples, p=weights))
+    centroids = samples[centroid_inds]
+    diffs = samples[:,np.newaxis]-centroids # diffs[i][j] is samples[i]-centroids[j].
+    squared_distances = np.sum(diffs**2,axis=2)
+    min_squared_distances = np.min(squared_distances,axis=1)
+    probs = min_squared_distances/np.sum(min_squared_distances)
+    centroid_inds.append(np.random.choice(num_samples, p=probs))
 
 print(",".join(map(str, centroid_inds)))
 mks.set_dim(dim)
