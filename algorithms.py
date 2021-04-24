5cee7196458a5cdd84019aef7564fff2600f5742
Maayan Kestenberg <maayan10.k@gmail.com>
2021-04-24 03:09:22 +0300

    Add (untested) code for all leftover algorithms.
    The only thing remaining to do in the spectral clustering function is
    add the call to k-means.

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,19 @@
 import numpy as np
+EPSILON = 0.0001
 
+# TODO: use the actual soft_assert
+def soft_assert(x,msg):
+    assert x,msg
+
+def assert_sqmat(mat):
+    return mat.ndim==2 and mat.shape[0]==mat.shape[1]
+
+def print_vars(vardict, equal_sep=' = ', sep=', '):
+    for name,val in vardict.items():
+        print(f"{name}{equal_sep}{val}{sep}\n")
+
+def print_multline_vars(vardict):
+    print_vars(vardict, ':\n', '\n')
 
 def diff_mat(a, b):
     return a[:, np.newaxis] - b
@@ -31,9 +45,6 @@ def weight_adj_mat(samples):
 # 	weight_row_sums = np.sum(weight_adj_mat(samples),axis=0)
 # 	return np.diag(weight_row_sums)
 
-# def norm_graph_lap_old(samples):
-# 	return
-
 
 def row_sums(mat):
     return np.sum(mat, axis=0)
@@ -62,28 +73,76 @@ def norm_graph_lap(samples):
     # as performing the matrix multiplication.
     diag_deg_and_weight_prod = weights * rsqrt_row_sums * rsqrt_row_sums[:, np.newaxis]
 
-    # TODO: consider swapping identity matrix calculation with fill_diagonal(1-diagonal)
+    # TODO: consider optimizing by swapping identity matrix calculation with fill_diagonal(1-diagonal)
     return np.identity(len(weights)) - diag_deg_and_weight_prod
 
 
 # Mutates the parameter mat!
 def qr_decomposition_destructive(mat):
+    dim = len(mat)
     u = mat.transpose()
     r = np.empty(u.shape)
     q = np.empty(u.shape)
-    for i in range(len(u)):
+    print_multline_vars({'u':u,'r':r,'q':q})
+    for i in range(dim):
         norm = np.linalg.norm(u[i])
-        r[i][i] = norm
-        normalized = q[i] = u[i] / norm
+        r[i,i] = norm
+        print_vars({'norm':norm})
+
+        # Exit on r[i,i]==0 as instructed on the forum.
+        soft_assert(norm!=0, "encountered r[i,i]=0 in qr decomposition!")
+        normalized = q[i] = u[i] / norm #if norm != 0 else np.zeros(dim)
+        print_vars({'normalized':normalized})
+        print_vars({'i':i})
+        print_multline_vars({'u':u,'r':r,'q':q})
 
         # for j in range(i+1,len(u)):
 
         # 	prod = np.inner(q[i],u[j])
-        # 	r[i][j] = prod
+        # 	r[i,j] = prod
         # 	u[j] -= prod*q[i]
-        prods = r[i][i + 1 :] = np.inner(normalized, u[i + 1 :])
-        print(f"prods={prods}, i={i}, r={r}")
-        u[i + 1 :] -= prods * q[i]
+        #
+        prods_calc = np.inner(normalized, u[i + 1 :])
+        print_multline_vars({'prods-calc':prods_calc})
+        prods = r[i,i + 1 :] = np.inner(normalized, u[i + 1 :])
+        print_multline_vars({'prods':prods, 'r':r})
+        u[i + 1 :] -= prods[:, np.newaxis] * q[i]
 
     return q.transpose(), r
 
+
+def qr_iteration(mat):
+    dim = len(mat)
+    e_val_mat = mat
+    e_vec_mat = np.identity(dim)
+    for _ in range(dim):
+       q,r = qr_decomposition_destructive(e_val_mat)
+       e_val_mat = r@q
+       mat_prod = e_vec_mat@q
+       if np.all(np.abs(np.abs(q)-np.abs(mat_prod))<=EPSILON):
+           return e_val_mat,e_vec_mat
+       e_vec_mat = mat_prod
+    return e_val_mat,e_vec_mat
+
+def normalize_rows(mat):
+   mat /= np.linalg.norm(mat,axis=-1)[:,np.newaxis]
+
+def norm_spectral_cluster(samples):
+    dim = len(samples)
+    l = norm_graph_lap(samples)
+
+    # TODO: rename e_val_mat to e_val_c_mat and e_vec_mat to e_vec_d_mat
+    # (for columns and diagonal, respectively)
+    e_val_mat,e_vec_mat = qr_iteration(l)
+    e_vals = e_val_mat.diagonal()
+
+    # TODO: consider optimizing with np.argpartition(e_vals, np.arange(dim/2)) instead of full sort
+    e_vals_first_half_sort_inds = np.argsort(e_vals)[:dim//2+1]
+    e_vals_sorted_first_half = e_vals[e_vals_first_half_sort_inds]
+    e_gaps = np.abs(np.diff(e_vals_sorted_first_half))
+    k = np.argmax(e_gaps)
+    relevant_e_vec_inds = e_vals_first_half_sort_inds[:k]
+    u = e_vec_mat[:,relevant_e_vec_inds]
+
+    normalize_rows(u)
+    # TODO: use kmeans on u.
