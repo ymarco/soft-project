b0c5d835bae6896c45b8bfe0afdccbfc939ece89
Maayan Kestenberg <maayan10.k@gmail.com>
2021-04-27 01:43:43 +0300

    Optimize and document qr algorithms.
    The optimization performed is using out parameters in order to write
    into constant buffers instead of allocating new memory.

--- algorithms.py
+++ algorithms.py
@@ -1,18 +1,20 @@
 import numpy as np
 import kmeans_numpy
 EPSILON = 0.0001
-DEBUG = False
 
 import debug_utils
 dbg = debug_utils.debug_printer(False)
 
 import numpy_utils
 
-# TODO: use the actual soft_assert
-def soft_assert(cond,msg):
-    assert cond,msg
-
 def weight_adj_mat(samples):
+    """
+    Calculate the weight adjacency matrix.
+    The resulting matrix ret is a symmetric matrix with diagonal of 0,
+    (ret[i][i]=0) and for i!=j, ret[i][j] is equal to:
+        exp(-l2_norm(samples[i]-samples[j])/2)
+    """
+
     # TODO: optimize this by avoiding calculation of symmetric elements
     # twice and avoiding calculation for points of the same index
     # (which we know are zeros on the diagonal).
@@ -22,20 +24,6 @@ def weight_adj_mat(samples):
     np.fill_diagonal(res, 0)
 
     return res
-
-
-# def weight_row_sums(samples):
-#     return np.sum(weight_adj_mat(samples),axis=0)
-
-# def diag_deg_mat(samples):
-#     weight_row_sums = np.sum(weight_adj_mat(samples),axis=0)
-#     return np.diag(weight_row_sums)
-
-
-
-def rsqrt_diag_deg_mat(samples):
-    return np.diag(weight_row_sums(samples) ** (-0.5))
-
 
 # 3.2, 3.3
 def norm_graph_lap(samples):
@@ -49,22 +37,64 @@ def norm_graph_lap(samples):
     # This is the diagonal of D as shown in step 3.1
     rsqrt_row_sums = numpy_utils.row_sums(weights) ** (-0.5)
 
-    # This is the calculation of D**(-0.5)@W@D (@ is matrix multplication) with D being the diagonal degree matfrom step 3.3.
-    # In order to reduce space and performance costs, we can avoid calculating and storing the actual diagonal
-    # matrix D with useless zeroes. We do that by multiplying the columns of W by D's diagonal and then
-    # multiplying the result's rows by the diagonal. Since D is a diagonal matrix this is exactly the same
-    # as performing the matrix multiplication.
+    # This is the calculation of D**(-0.5)@W@D with D being the diagonal
+    # degree matrix from step 3.3.
+    # In order to reduce space and performance costs, we can avoid
+    # calculating and storing the actual diagonal matrix D with useless
+    # zeroes. We do that by multiplying the columns of W by D's diagonal
+    # and then multiplying the result's rows by the diagonal. Since D is
+    # a diagonal matrix this is exactly the same as performing the matrix
+    # multiplication.
     diag_deg_and_weight_prod = (weights * rsqrt_row_sums) * rsqrt_row_sums[:, np.newaxis]
 
     # TODO: consider optimizing by swapping identity matrix calculation with fill_diagonal(1-diagonal)
     return np.identity(len(weights)) - diag_deg_and_weight_prod
 
 
-def qr_decomposition(mat):
+def qr_decomposition(mat, out = (None, None) ,destructive=False, assume_out_r_is_up_tri=False):
     """
     Returns an orthogonal matrix q and an upper triangular matrix r
     such that r@q==mat.
+
+    Contains an optional out parameter for specifying arrays to write
+    the result into instead of allocating new ones.
+
+    The parameter destructive, when true, allows the algorithm to
+    manipulate the input array instead of copying it for its purposes.
+
+    The parameter assume_out_r_is_up_tri is for optimization and should
+    be set to true only if the caller can guarantee that the second out
+    argument given is already upper triangular. Setting it to true lets
+    the algorithm avoid zeroing the array. If set to true despite having
+    a non upper triangular out parameter for r the returned r will not be
+    upper triangular either and the algorithm will be incorrect.
+
+    Whenever possible, it is recommended to supply a column-major
+    (fortran-style) array when supplying the input matrix mat and the
+    first out argument (q). This is because the algorithm uses
+    operations which access elements in columns of q sequentially (and
+    in this case column-major offers better cache locality).
     """
+    out_q,out_r = out
+    if out_q is None:
+        # Column-major:
+        out_q = np.empty(mat.shape,order='F')
+    if out_r is None:
+        out_r = np.empty(mat.shape)
+    assert out_q.shape == out_r.shape == mat.shape
+    if not assume_out_r_is_up_tri:
+        out_r.fill(0)
+
+    # TODO: assert that the given argument mat is always symmetric
+    # in our code, and optimize by not transposing it at all, using:
+
+
+    # For syntactic perference, we transpose u and q and use their
+    # rows instead of columns (for readability/writeability).
+    q = out_q.T
+    r = out_r
+    u = mat.T if destructive else mat.T.copy()
+
     dbg = debug_utils.debug_printer(False)
     dbg2 = debug_utils.debug_printer(False)
     if dbg2.is_active() or True:
@@ -73,16 +103,7 @@ def qr_decomposition(mat):
     if dbg2.is_active() or True:
         expected_q,expected_r = np.linalg.qr(mat)
 
-    # TODO: assert that the given argument mat is always symmetric
-    # in our code, and optimize by not transposing it at all, using:
 
-    # In this implementation we transpose both u and q, this way
-    # we can manipulate their rows instead of columns (which is
-    # faster and more readable)
-    u = mat.transpose().copy()
-
-    q = np.empty(u.shape)
-    r = np.zeros(u.shape)
     dbg.print_multiline_vars({'u':u,'r':r,'q':q})
     dbg2.print_multiline_vars({'u':u,'r':r,'q':q})
     for i in range(dim):
@@ -97,7 +118,7 @@ def qr_decomposition(mat):
 
         # Exit on r[i,i]==0 as instructed on the forum.
         if norm==0:
-           raise RuntimeError("Encountered R[i,i]=0 in qr decomposition!")
+           raise RuntimeError("Encountered R[i,i]=0 in qr decomposition.")
 
         dbg.print_multiline_vars({'q[i]':q[i], 'u[i]':u[i], 'norm':norm})
         normalized = q[i] = u[i] / norm #if norm != 0 else np.zeros(dim)
@@ -119,49 +140,71 @@ def qr_decomposition(mat):
         dbg2.print_multiline_vars({'prods[:, np.newaxis] * q[i]':prods[:, np.newaxis] * q[i]})
         remaining_vecs -= prods[:, np.newaxis] * q[i]
 
-    # Return the transpose of q because we were looking at the transposed 
-    # version up until now.
-    q=q.transpose()
     dbg2.print("calculated q & r:")
-    dbg2.print_multiline_vars({'expected_q':expected_q, 'q':q,
-     'q.T@q':q.T@q, 'q@r':q@r, 'original mat':mat_copy, 'r':r,
-      'expected_r':expected_r, 'expected_q,q diff':np.abs(expected_q)-np.abs(q)})
-    return q, r
+    dbg2.print_multiline_vars({'expected_q':expected_q, 'q':out_q,
+     'q.T@q':q@out_q, 'q@r':out_q@r, 'original mat':mat_copy, 'r':r,
+      'expected_r':expected_r, 'expected_q,q diff':np.abs(expected_q)-np.abs(out_q)})
+    return out_q, out_r
 
 
-def qr_iteration(mat):
+def qr_iteration(mat, destructive=False):
+    """
+    Returns an orthogonal matrix whose columns approach the eigenvectors
+    of the input matrix mat and a matrix whose diagonal elements approach
+    the eigenvalues of mat.
+
+    The parameter destructive, when true, allows the algorithm to
+    manipulate the input array instead of copying it for its purposes.
+    """
 
     #TODO: optimization - consider using constant buffers instead
     # of allocating new arrays (for example make e_val_mat have a
     # constant place in memory).
 
     dbg = debug_utils.debug_printer(False)
+
     dim = len(mat)
-    e_val_mat = mat
+    e_val_mat = mat if destructive else mat.copy()
     e_vec_mat = np.identity(dim)
+    q = np.empty(mat.shape)
+    r = np.zeros_like(q)
+    temp_mat_prod_b = np.empty(mat.shape)
     for i in range(dim):
         dbg.print_multiline_vars({
             'qr_iteration iteration number':i,
             'e_val_mat':e_val_mat,
             'e_vec_mat':e_vec_mat,
             })
-        q,r = qr_decomposition(e_val_mat)
-        e_val_mat = r@q
-        mat_prod = e_vec_mat@q
-
-        is_close_to_convergence = np.allclose(abs(e_vec_mat), abs(mat_prod), atol=EPSILON, rtol=0)
-        dbg.print_multiline_vars({
-            'e_val_mat (after change)':e_val_mat,
-            'mat_prod':mat_prod,
-            'less than epislon cond':is_close_to_convergence
-            })
+        qr_decomposition(
+            e_val_mat,
+            out = (q,r),
+            destructive=True,
+            assume_out_r_is_up_tri=True
+        )
+        np.matmul(r,q,
+                  out=e_val_mat)
+        np.matmul(e_vec_mat, q,
+                  out=temp_mat_prod_b)
 
         # Checking if we're close enough to convergence.
         # The parameter rtol is for relative tolerance, setting that to zero
-        # makes the comparison non relative. atol=EPSILON is our absoloute tolerance as wanted. 
+        # makes the comparison non relative. atol=EPSILON is our absoloute tolerance as wanted.
+        is_close_to_convergence = np.allclose(
+            abs(e_vec_mat), abs(temp_mat_prod_b),
+            atol=EPSILON, rtol=0)
+        dbg.print_multiline_vars({
+            'e_val_mat (after change)':e_val_mat,
+            'temp_mat_prod_b':temp_mat_prod_b,
+            'less than epislon cond':is_close_to_convergence
+            })
+
         if is_close_to_convergence:
             return e_val_mat,e_vec_mat
-        e_vec_mat = mat_prod
+
+        # Set e_vec_mat = temp_mat_prod_b and swap the buffer
+        # so temp_mat_prod_b will be free to use withotu affecting
+        # e_vec_mat.
+        e_vec_mat, temp_mat_prod_b = temp_mat_prod_b , e_vec_mat
     return e_val_mat,e_vec_mat
 
 # If k==None, uses the eigengap heuristic
